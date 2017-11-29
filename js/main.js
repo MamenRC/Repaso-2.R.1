1fbc97326974bcc241fb8aafb84a2b2348b9518e
Mamen <mamenrueco@gmail.com>
2017-11-29 10:08:17 +0100

    repaso1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var user;
+var need;
+var reason;
+var story;
+function complete() {
+	user = prompt('¿Quién eres?');
+	need = prompt('¿Qué necesitas?');
+	reason = prompt('¿Pá qué?');
+	story = {user: user, need: need, reason: reason};
+
+	var result = 'yo como ' + story.user + ' ,quiero' + story.need + ' para ' + story.reason;
+	return result;
+
+}
+alert (complete());
